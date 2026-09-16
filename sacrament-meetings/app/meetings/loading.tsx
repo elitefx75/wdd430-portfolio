1e2add4fca9ba070eb1164794f2c46c83b421e4d
elitefx75 <pbwire@byupathway.edu>
2026-09-16 08:21:26 +0300

    updated

--- sacrament-meetings/app/meetings/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-    return <div aria-label="Loading meetings"><div className="loading-line" /><div className="loading-line" style={{ marginTop: "1rem", width: "70%" }} /></div>;
-}
